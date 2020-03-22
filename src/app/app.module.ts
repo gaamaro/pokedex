79e1a6aed939841013f4dcc3541888d5a8db5cbb
gaamaro <amaro.gabriel@live.com>
2020-03-22 18:00:14 -0300

    Increase Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { RollOnScrollDirective } from './directives/roll-on-scroll.directive';
+import { PokenumberPipe } from './pipes/pokenumber.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     ListItemComponent,
-    RollOnScrollDirective
+    RollOnScrollDirective,
+    PokenumberPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/pokenumber.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PokenumberPipe } from './pokenumber.pipe';
+
+describe('PokenumberPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PokenumberPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
